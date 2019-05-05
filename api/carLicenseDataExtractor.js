8b6a7e45674743cc31c0e011d9f1bb17b3b3a9b2
Fady Saeed <fady.saeed@outlook.com>
2019-05-05 21:57:59 +0200

    Postprocess car license data retrieved from the server

--- api/carLicenseDataExtractor.js
+++ api/carLicenseDataExtractor.js
@@ -15,8 +15,28 @@ export const getCarLicenseData = async (image) => {
     const response = await fetch(API_URL, options)
     const responseJSON = await response.json()
 
-    if(responseJSON.type === "success")
-        return responseJSON.payload
-    else
+    if(responseJSON.type === "success") {
+        responseJSON.payload.letters = responseJSON.payload.letters.map(lettersPlate => {
+            return {
+                firstLetter: lettersPlate[0],
+                secondLetter: lettersPlate[1],
+                thirdLetter: lettersPlate[2]
+            }
+        })
+        
+        const licensePlatesCombinations = []
+        for(let ithLetters=0; ithLetters < responseJSON.payload.letters.length; ithLetters++) {
+            for(let ithDigits=0; ithDigits < responseJSON.payload.digits.length; ithDigits++) {
+                licensePlatesCombinations.push({
+                    firstLetter: responseJSON.payload.letters[ithLetters].firstLetter,
+                    secondLetter: responseJSON.payload.letters[ithLetters].secondLetter,
+                    thirdLetter: responseJSON.payload.letters[ithLetters].thirdLetter,
+                    digits: responseJSON.payload.digits[ithDigits]
+                })
+            }
+        }
+        return licensePlatesCombinations
+    } else {
         throw new Error(responseJSON.message)
+    }
 }
